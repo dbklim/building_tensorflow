0f87fa60b2aa1117caaa01987dbfe539442eb3ff
Desklop <vladsklim@gmail.com>
2019-06-25 21:01:50 +0300

    Added project files

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Сборка и установка ngraph работает крайне коряво, получилось завести версию r0.14 и получил наоборот, замедление работы где-то на 20-30%, вместо обещанного ускорения до х10 раз
+# Для нормальной сборки надо cmake и python3-venv
+# Собранный пакет ngraph работает только с указанной в ридми версией tensorflow, если подсунуть свой собранный tensorflow той же версии - ошибка, если в процессе сборки ngraph указать ему сборку и tensorflow - так же ошибка
+
+#git clone https://github.com/tensorflow/ngraph-bridge.git
+#cd ngraph-bridge
+#git checkout r0.14
+#python3 build_ngtf.py --use_prebuilt_tensorflow
+#cp build_cmake/artifacts/ngraph_tensorflow_bridge*.whl /mnt
+#cd /build_tensorflow
+
+echo -e "\nSTEP 1 Preparation\n"
+cd tensorflow
+git checkout r1.13
+
+echo -e "\nSTEP 2 Configure build\n"
+if [[ $1 = 'gpu' ]]
+then 
+    ./run_configure_gpu_1.13.1
+else 
+    ./run_configure_cpu_1.13.1
+fi
+
+echo -e "\nSTEP 3 Build\n"
+#bazel build --config=opt --config=mkl --noincompatible_strict_action_env //tensorflow/tools/pip_package:build_pip_package
+bazel build --config=opt --noincompatible_strict_action_env //tensorflow/tools/pip_package:build_pip_package
+
+echo -e "\nSTEP 4 Create wheel\n"
+CPU_MODEL=$(cat /proc/cpuinfo | grep -m 1 "model name" | grep -o "\S[0-9]\?-\?[0-9]\{3,5\}\S" | sed "s/\s//g")
+if [[ $1 = 'gpu' ]]
+then 
+    GPU_MODEL=$(nvidia-smi | grep -o -m 1 "GeForce\s\S*\s[0-9]\{0,4\}" | sed "s/\s//g")
+    NAME="tensorflow-$CPU_MODEL-$GPU_MODEL"
+else 
+    NAME="tensorflow-$CPU_MODEL"
+fi
+./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt --project_name $NAME
+
+echo -e "\nDone. See $NAME-1.13.1-cp36-cp36m-linux_x86_64.whl\n"
---
+++ install_bazel.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo_help() {
+    echo "Выполняет загрузку и установку указанной версии Bazel."
+    echo "Использование: [-h] [help] version"
+    echo "Пример:"
+    echo "./install_bazel.sh 0.21.0"
+    echo "./install_bazel.sh 0.24.1"
+}
+
+BAZEL_VERSION="0.21.0"
+
+if [ -n "$1" ]; then
+    case "$1" in
+        -h) echo_help
+            exit;;
+        help) echo_help
+            exit;;
+    esac
+    
+    if echo $1 | grep -c "[0-9]\{1,2\}\.[0-9]\{1,2\}\.[0-9]\{1,2\}" > /dev/null; then
+        BAZEL_VERSION=$1
+    else            
+        echo "Некорректное значение version, введите -h или help для помощи"
+        exit
+    fi
+else
+    echo_help
+    exit
+fi
+
+if [ ! -f "bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh" ]; then
+    wget https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
+else            
+    echo -e "Found: bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh\n"
+fi
+
+chmod +x bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
+./bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
+echo "Done"
